2dc9f4a0e39dd92cad1433d7b7d870d976e32134
Mohamoud-Jire <amoh54932@gmail.com>
2023-07-18 07:14:56 +0300

    Answered

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+function calcTip(bill) {
+  let tip;
+  if(bill < 300 && bill > 50) {
+    tip = 0.15;
+  } else {
+    tip = 0.2;
+  }
+  return bill * tip;
+}
+let bill = 301;
+let tip = calcTip(bill);
+let total = bill + tip;
+console.log(`the bill was ${bill}, the tip was ${tip}, and the total is ${total}`);
